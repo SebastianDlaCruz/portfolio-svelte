0a55d396b777263294a7b682ed9f3450951283d5
estudiandev <henrrybrgt@gmail.com>
2023-04-05 09:07:28 -0400

    initial commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,10 @@
+<header class="bg-transparent backdrop-blur-sm sticky top-0 flex justify-between max-w-screen-xl mx-auto p-10">
+	<h1 class="text-3xl">Henrry Bourgeot</h1>
+	<nav class="flex justify-around w-1/2 items-center">
+		<a href="#about">About Me</a>
+		<a href="#exp">Experience</a>
+		<a href="#projects">Projects</a>
+		<a href="https://estudiandev.site">Blog</a>
+		<a href="#contact">Contact</a>
+	</nav>
+</header>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+import "virtual:windi.css"
+import Header from "$lib/components/Header.svelte";
+import gopherXl from "$lib/assets/gopher-xl.png";
+import gopherLg from "$lib/assets/gopher-lg.png";
+let x: number;
+let y: number;
+
+</script>
+<svelte:head>
+	<title>I'm Henrry Bourgeot</title>
+</svelte:head>
+<svelte:window bind:innerWidth={x} bind:innerHeight={y}/>
+<div>
+	<Header/>
+	<main class={`${x < 768 ? "flex-col" : ""} flex justify-center max-w-screen-xl mx-auto items-center`} style={`--height: ${x<768 ? y-50 : y-120}px`}>
+		<section class="w-1/2 flex flex-col gap-3 p-6 justify-center">
+			<h3 class="text-xl font-bold">Hi there! I'm</h3>
+			<h2 class="text-5xl mb-6">Henrry Bourgeot, a Fullstack Developer based in Maracay, Venezuela</h2>
+			<p class="text-xl mb-12">I have professional experience working with Svelte, Python, Go, and other languages and frameworks that you will find in the <a href="#skills">skills section</a> of this page.</p>
+			<a href="#projects" class="border-4 border-blue-600 mx-auto ml-0 py-3 px-7">See projects</a>
+		</section>
+		{#if x > 768}
+			<img src={gopherXl} class="w-150" alt="Gopher"/>	
+		{:else}
+			<img src={gopherLg} class="w-150" alt="Gopher"/>
+		{/if}
+	</main>
+
+	<section id="about">
+		<h2 class="text-xl font-bold">About me</h2>
+	</section>
+</div>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;600&display=swap');
+	* {
+		font-family: 'Raleway', sans-serif;
+	}
+
+	main.justify-center{
+		height: var(--height);
+	}
+
+</style>
